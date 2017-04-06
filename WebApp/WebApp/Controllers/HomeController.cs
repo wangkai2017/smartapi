02f923ae8f09be7d9519aaf08b5ef8423a19d8ed
王凯 <wk33433@ly.com>
2017-04-06 18:12:23 +0800

    .

--- WebApp/WebApp/Controllers/HomeController.cs
+++ WebApp/WebApp/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Chloe.SqlServer;
+using SmartCommon.DbHelper;
+using SmartCommon.SerializerHelper;
+using SmartEntity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +12,13 @@ namespace WebApp.Controllers
 {
     public class HomeController : Controller
     {
+        /* DbContext 是非线程安全的，正式使用不能设置为 static */
+        private MsSqlContext context = new MsSqlContext(DbHelper.ConnectionString);
         public ActionResult Index()
         {
+            var entity = context.Query<Students>();
+            var str = SerializerHelper.ObjectToJson(entity.ToList());
+            ViewBag.Str = str;
             return View();
         }
 
